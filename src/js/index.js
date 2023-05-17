74cb24b28ecb6ffb11f1033b2570ffafc317fff4
Cat <126123826+CRob001@users.noreply.github.com>
2023-05-17 22:23:45 +0000

    small change

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "../styles/index.css";
 import Layout from './layout.js'
 
 //
-const root = createRoot(document.querySelector("#app"))
+const root = createRoot(document.querySelector("#ap"))
 
 //render your react application
 root.render(<Layout/>)
